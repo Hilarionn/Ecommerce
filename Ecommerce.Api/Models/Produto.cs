d2fb2303c9c08cc70d7011d129ef130b91424d32
Gabriel Hilarion <hilarion.gabriel.fr@gmail.com>
2026-07-28 16:48:55 -0300

    Salvando status atual

---
+++ Ecommerce.Api/Models/Produto.cs
@@ -0,0 +1,18 @@
+namespace Ecommerce.Api.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = string.Empty;
+
+    public string Descricao { get; set; } = string.Empty;
+
+    public decimal Preco { get; set; }
+
+    public int EstoqueQuantidade { get; set; }
+
+    public int CategoriaId { get; set; }
+
+    public Categoria? Categoria { get; set; }
+}
