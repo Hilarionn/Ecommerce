667c0a005e1b74b5a3feec8657b3bbba55291aaf
Gabriel Hilarion <hilarion.gabriel.fr@gmail.com>
2026-07-29 13:41:17 -0300

    Configura Entity Framework Core com SQLite e cria banco inicial

--- Ecommerce.Api/Program.cs
+++ Ecommerce.Api/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Ecommerce.Api.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
