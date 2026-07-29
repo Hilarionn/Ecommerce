320096c544f83909c202a06a2e371adbb25cd50c
Gabriel Hilarion <hilarion.gabriel.fr@gmail.com>
2026-07-29 08:03:17 -0300

    Adicionando Categoria

---
+++ Ecommerce.Api/Models/Categoria.cs
@@ -0,0 +1,10 @@
+namespace Ecommerce.Api.Models;
+
+public class Categoria
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = string.Empty;
+
+    public List<Produto> Produtos { get; set; } = new();
+}
